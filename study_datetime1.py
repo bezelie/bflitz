e203db10392755b77a178c4dd0b8ee3fa82d48e0
bezelie <nuja@live.jp>
2019-02-06 18:24:20 +0900

    m

---
+++ study_datetime1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Study Code : datetimeの使いかた
+# listを定義し、代入し、表示する
+
+import datetime
+
+print (datetime.date.today())
+print (datetime.datetime.now())
---
+++ study_random1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Learning Code : randomモジュールの使いかた
+# ３つの選択肢からランダムでひとつ選ぶ
+
+import random
+data = ["A","B","C"]
+print (random.choice(data))
+
