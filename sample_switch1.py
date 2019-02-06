a6de3c3e971a22be694fdbed469d9096d114a8f7
bezelie <nuja@live.jp>
2019-02-06 18:36:42 +0900

    m

---
+++ sample_switch1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Sample Code : スイッチ（＋プルダウン抵抗）入力のサンプル
+# マニュアルを見てラズパイにスイッチを接続しておいてください。
+
+# モジュールのインポート
+from time import sleep                 # sleep(ウェイト処理)ライブラリの読み込み
+import RPi.GPIO as GPIO                # GPIO(汎用入出力端子)ライブラリの読み込み
+
+# 変数と定数
+pinSwitch = 4
+
+# 初期設定
+GPIO.setmode(GPIO.BCM)                 # GPIOをGPIO番号で指定できるように設定
+GPIO.setup(pinSwitch, GPIO.IN)         # GPIOの4ピンを入力モードに設定
+
+# 関数
+def main():
+  try:
+    print ("開始します")
+    while True:                            # 繰り返し処理
+      if GPIO.input(pinSwitch)==GPIO.HIGH: # GPIO4に3.3Vの電圧がかかっていたら・・・
+        print ("スイッチが押されています")
+      else:                                # それ以外の場合は・・・
+        print ("押されてません")
+      sleep (0.5)                          # 0.5秒待つ
+  except KeyboardInterrupt:                # コントロール＋Cが押された場合の処理
+    print ("終了しました")
+    GPIO.cleanup()                         # ポートをクリア
+
+# 直接実行された場合の処理
+if __name__ == "__main__":
+    main()
